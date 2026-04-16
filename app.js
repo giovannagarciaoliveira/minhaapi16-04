a7bded8d85444957d8171c71baf92d90292a23be
giovannagarciaoliveira <giovannagarciaoliveira6@gmail.com>
2026-04-16 15:29:32 -0300

    feat:adicionar configuração de rotas

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+
+import plantRoutes from './src/routes/plantRoutes.js';
+import typesRoutes from './src/routes/typesRoutes.js'
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/plants', plantRoutes);
+app.use('/types', typesRoutes);
+
+export default app;
+
